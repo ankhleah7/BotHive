2dd7c0507334fbf94d7bb293168cb432e16f68e4
Alvin Mites <alvin@mitesdesign.com>
2016-10-15 13:37:01 -0600

    removed cluster url

--- BotHive/urls.py
+++ BotHive/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^cluster/', include('cluster.urls')),
     url(r'^bots/', include('bots.urls')),
     url(r'^jobs/', include('jobs.urls')),
     url(r'^admin/', admin.site.urls),
